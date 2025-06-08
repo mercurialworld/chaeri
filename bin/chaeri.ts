0e12fcac8e459117f825b1f033ecf7aa1a279c29
ポッチャマ <mercworld@pocha.moe>
2025-06-07 20:42:53 -0400

    ci: add box

--- bin/chaeri.ts
+++ bin/chaeri.ts
@@ -6,6 +6,7 @@ const app = new cdk.App();
 new ChaeriStack(app, "ChaeriStack", {
     env: { account: "575108959833", region: "us-east-1" },
     repos: [
+        { owner: "mercurialworld", repo: "dreamcatching", githubEnv: "aws-cdk" },
         { owner: "mercurialworld", repo: "chaeri", githubEnv: "aws-cdk" },
         { owner: "mercurialworld", repo: "mafuyu", githubEnv: "aws-cdk" },
         { owner: "mercurialworld", repo: "pochasite", githubEnv: "aws-cdk" },
