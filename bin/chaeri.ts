2400542d7f1ab9e58b892b4be264a55436ff1550
pochama <mercworld@pocha.moe>
2025-08-28 20:08:08 -0400

    feat: add mercurialworld/pochamoe-api

--- bin/chaeri.ts
+++ bin/chaeri.ts
@@ -11,5 +11,6 @@ new ChaeriStack(app, "ChaeriStack", {
         { owner: "mercurialworld", repo: "mafuyu", githubEnv: "aws-cdk" },
         { owner: "mercurialworld", repo: "pochasite", githubEnv: "aws-cdk" },
         { owner: "mercurialworld", repo: "media-players", githubEnv: "aws-cdk" },
+        { owner: "mercurialworld", repo: "pochamoe-api", githubEnv: "aws-cdk" },
     ],
 });
